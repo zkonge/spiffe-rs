e37ef7763b851b36dbcdc7c84206f2971bca90a6
Konge <zkonge@outlook.com>
2026-02-19 14:44:48 +0800

    prefer serde-core

--- spiffe/src/jwt.rs
+++ spiffe/src/jwt.rs
@@ -1,10 +1,73 @@
-use std::borrow::Cow;
+use std::fmt::{Formatter, Result as FmtResult};
 
 use base64ct::{Base64UrlUnpadded, Encoding};
-use serde::Deserialize;
+use serde_core::{
+    Deserialize, Deserializer,
+    de::{Error, IgnoredAny, MapAccess, Visitor},
+};
+use serde_json::Deserializer as JsonDeserializer;
 use spiffe_id::SpiffeId;
 
 use crate::SpiffeError;
+
+struct SubIdentifierVisitor;
+
+impl Visitor<'_> for SubIdentifierVisitor {
+    type Value = IsSub;
+
+    fn expecting(&self, formatter: &mut Formatter) -> FmtResult {
+        formatter.write_str("field identifier")
+    }
+
+    fn visit_str<E: Error>(self, v: &str) -> Result<Self::Value, E> {
+        Ok(IsSub(v == "sub"))
+    }
+
+    fn visit_bytes<E: Error>(self, v: &[u8]) -> Result<Self::Value, E> {
+        Ok(IsSub(v == b"sub"))
+    }
+}
+
+struct IsSub(bool);
+
+impl<'de> Deserialize<'de> for IsSub {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_identifier(SubIdentifierVisitor)
+    }
+}
+
+struct SubClaimVisitor;
+
+impl<'de> Visitor<'de> for SubClaimVisitor {
+    type Value = Box<str>;
+
+    fn expecting(&self, formatter: &mut Formatter) -> FmtResult {
+        formatter.write_str("claim with field `sub`")
+    }
+
+    fn visit_map<A: MapAccess<'de>>(self, mut map: A) -> Result<Self::Value, A::Error> {
+        let mut sub = None;
+
+        while let Some(is_sub) = map.next_key()? {
+            match is_sub {
+                IsSub(true) => {
+                    if sub.is_some() {
+                        return Err(Error::duplicate_field("sub"));
+                    }
+                    sub = Some(map.next_value()?);
+                }
+                IsSub(false) => {
+                    map.next_value::<IgnoredAny>()?;
+                }
+            }
+        }
+
+        match sub {
+            Some(sub) => Ok(sub),
+            None => Err(Error::missing_field("sub")),
+        }
+    }
+}
 
 /// Extracts SPIFFE ID from a trusted JWT-SVID
 ///
@@ -13,19 +76,15 @@ use crate::SpiffeError;
 /// Usually, this function is used to extract SPIFFE ID from a JWT-SVID that is already verified.
 pub fn spiffe_id_from_jwt_svid_unchecked(svid: &str) -> Result<SpiffeId, SpiffeError> {
     const INVALID_JWT_ERR: SpiffeError = SpiffeError::InvalidJwtSvid;
-    let (prefix, _signature) = svid.split_once('.').ok_or(INVALID_JWT_ERR)?;
-    let (_header, body) = prefix.split_once('.').ok_or(INVALID_JWT_ERR)?;
+
+    let (prefix, _signature) = svid.rsplit_once('.').ok_or(INVALID_JWT_ERR)?;
+    let (_header, body) = prefix.rsplit_once('.').ok_or(INVALID_JWT_ERR)?;
 
     let body_json = Base64UrlUnpadded::decode_vec(body).map_err(|_| INVALID_JWT_ERR)?;
 
-    #[derive(Deserialize)]
-    struct SubjectExtractor<'a> {
-        #[serde(borrow)]
-        sub: Cow<'a, str>,
-    }
+    let sub: Box<str> = JsonDeserializer::from_slice(&body_json)
+        .deserialize_struct("Claims", &["sub"], SubClaimVisitor)
+        .map_err(|_| INVALID_JWT_ERR)?;
 
-    let extactor: SubjectExtractor<'_> =
-        serde_json::from_slice(&body_json).map_err(|_| INVALID_JWT_ERR)?;
-
-    SpiffeId::new(extactor.sub).map_err(SpiffeError::SpiffeId)
+    SpiffeId::new(sub).map_err(SpiffeError::SpiffeId)
 }
